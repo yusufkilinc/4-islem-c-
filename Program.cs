3132f159bd54ed54ad0344b9f8fe1a4cc1cb63db
yusufkilinc <119483659+yusufkilinc@users.noreply.github.com>
2023-01-05 11:27:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿            int sayi1,sayi2,sonuc;
+            Console.Write("1.Sayıyı Giriniz : ");
+            sayi1=Convert.ToInt32(Console.ReadLine());
+            Console.Write("2.Sayıyı Giriniz : ");
+            sayi2=Convert.ToInt32(Console.ReadLine());
+            sonuc=sayi1+sayi2;
+            Console.WriteLine("{0} + {1} = {2}",sayi1,sayi2,sonuc);
+            sonuc=sayi1-sayi2;
+            Console.WriteLine("{0} - {1} = {2}",sayi1,sayi2,sonuc);
+            sonuc=sayi1*sayi2;
+            Console.WriteLine("{0} * {1} = {2}",sayi1,sayi2,sonuc);
+            sonuc=sayi1/sayi2;
+            Console.WriteLine("{0} / {1} = {2}",sayi1,sayi2,sonuc);
